226c62a42f524c801cfca5c4dcf9829f0f08c711
Hadeel Yassin <hadeelyassin1997@gmail.com>
2021-02-14 21:40:53 +0200

    Second Assignment

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,4 +1,6 @@
 const ProductController = require('../controllers/products.controllers');
 module.exports = function(app){
     app.post('/api/product', ProductController.createProduct);
+    app.get('/api/allproducts', ProductController.getAllProduct);
+    app.get('/api/product/:id', ProductController.getProduct);
 }
